c19167547f0b6d07a3450b1521e9f9b741e5380e
Wesley Miller <37603175+wmiller4@users.noreply.github.com>
2024-06-06 23:07:47 -0500

    Fix provider filtering
    Filter operation was checking promises, which always evaluated to true.

--- src/environment.ts
+++ src/environment.ts
@@ -1,30 +1,39 @@
-import * as vscode from 'vscode';
 import { PythonExtension } from "@vscode/python-extension";
 import fs, { PathLike } from "fs";
 import path from "path";
+import * as vscode from 'vscode';
+import logger from './logger';
 import { VenvProvider } from "./providers";
 
+type PythonEnvironment = {
+	providerName: string
+	pythonPath: string
+};
+
 export class EnvironmentLookup {
-	private readonly environments = new Map<string, string | undefined>();
+	private readonly environments = new Map<string, PythonEnvironment | undefined>();
 	private readonly providers: VenvProvider[];
 
 	constructor(providers: VenvProvider[]) {
 		this.providers = providers;
 	}
 
-	private async findPython(cwd: string): Promise<string | undefined> {
+	private async findPython(cwd: string): Promise<PythonEnvironment | undefined> {
 		for (const provider of this.providers) {
 			const python = await provider.getPython(cwd).catch(_ => undefined);
 			if (python && fs.existsSync(python)) {
-				console.log(`Found ${provider.name} environment ${python} for ${cwd}`);
-				return python;
+				logger.debug(`Found ${provider.name} environment ${python} for ${cwd}`);
+				return { providerName: provider.name, pythonPath: python };
 			}
+			logger.debug(`No ${provider.name} environment found for ${cwd}`);
 		}
-		console.log(`No environment found for ${cwd}`);
 		return undefined;
 	}
 
-	async getPythonPath(sourceFile: PathLike, ignoreCache: boolean = false): Promise<string | undefined> {
+	async getPythonPath(
+		sourceFile: PathLike,
+		ignoreCache: boolean = false
+	): Promise<PythonEnvironment | undefined> {
 		const cwd = path.dirname(sourceFile.toString());
 		if (!this.environments.has(cwd) || ignoreCache) {
 			const python = await this.findPython(cwd);
@@ -32,20 +41,6 @@ export class EnvironmentLookup {
 		}
 		return this.environments.get(cwd);
 	}
-}
-
-export function createLookup(providers: VenvProvider[]): EnvironmentLookup {
-	const filtered = providers.filter(async provider => {
-		try {
-			await provider.isAvailable();
-			console.log(`Enabled ${provider.name}`);
-			return true;
-		} catch (_) {
-			console.log(`Disabled ${provider.name}`);
-			return false;
-		}
-	});
-	return new EnvironmentLookup(filtered);
 }
 
 export class EnvironmentManager {
@@ -65,14 +60,14 @@ export class EnvironmentManager {
 		if (!editor || editor.document.languageId !== 'python') { return; }
 		const targetEnvironment = await this.lookup.getPythonPath(editor.document.uri.fsPath, ignoreCache);
 		if (!targetEnvironment) {
-			console.log('No virtual environment found.');
+			logger.info('No virtual environment found.');
 			return;
 		}
-		if (this.activeEnvironment === targetEnvironment) {
-			console.log('Target virtual environment is already active.');
+		if (this.activeEnvironment === targetEnvironment.pythonPath) {
+			logger.debug('Target virtual environment is already active.');
 			return;
 		}
-		this.pythonApi.environments.updateActiveEnvironmentPath(targetEnvironment);
-		console.log(`Set environment to ${targetEnvironment}`);
+		this.pythonApi.environments.updateActiveEnvironmentPath(targetEnvironment.pythonPath);
+		logger.info(`Activated ${targetEnvironment.providerName} environment ${targetEnvironment.pythonPath}`);
 	}
 }
--- src/providers.ts
+++ src/providers.ts
@@ -1,12 +1,15 @@
 import * as cp from 'child_process';
+import logger from './logger';
 
 const exec = async (command: string, cwd?: string) => new Promise<string>((resolve, reject) => {
-	cp.exec(command, { cwd }, (err, out) => err ? reject(err) : resolve(out));
+    cp.exec(command, { cwd }, (err, out) => {
+        logger.trace('Executed shell command', command, err, out);
+        return err ? reject(err) : resolve(out);
+    });
 });
 
 export type VenvProvider = {
     name: string
-    isAvailable: () => Promise<void>
     getPython: (sourceDir: string) => Promise<string>
 };
 
@@ -16,26 +19,32 @@ export type ToolConfig = {
     python: string
 };
 
-export const toolProvider = (config: ToolConfig): VenvProvider => ({
-    name: config.name,
-    isAvailable: () => exec(config.check).then(_ => {}),
-    getPython: (sourceDir) => exec(config.python, sourceDir).then(py => py.trim())
-});
+export const toolProvider = (config: ToolConfig): Promise<VenvProvider> => (
+    exec(config.check).then(() => ({
+        name: config.name,
+        getPython: (sourceDir) => exec(config.python, sourceDir).then(py => py.trim())
+    }))
+);
 
-export const defaultProviders = [
-    toolProvider({
+const defaultProviders: ToolConfig[] = [
+    {
         name: 'PDM',
         check: 'pdm --version',
         python: 'pdm info --python'
-    }),
-    toolProvider({
+    },
+    {
         name: 'Poetry',
         check: 'poetry --version',
         python: 'poetry env info -e'
-    }),
-    toolProvider({
+    },
+    {
         name: 'Pipenv',
         check: 'pipenv --version',
         python: 'pipenv --py'
-    }),
+    },
 ] as const;
+
+export const availableProviders = async (providers: ToolConfig[] = defaultProviders) => {
+    const results = await Promise.allSettled(providers.map(toolProvider));
+    return results.flatMap(result => result.status === 'fulfilled' ? [result.value] : []);
+};
